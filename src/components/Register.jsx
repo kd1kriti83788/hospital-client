469e534b8070c015a5eaac697413fb2a08c19ab2
kriti <kritikeshri1@gmail.com>
2026-05-17 15:06:55 +0530

    register api

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,28 @@
 import "./Register.css";
+import { useRef } from "react";
+import {register} from "../services/authenticationService";
 
 function Register({ openLogin }) {
+    const nameRef=useRef("");
+    const emailRef=useRef("");
+    const mobileRef=useRef("");
+    const addressRef=useRef("");
+    const passwordRef=useRef("");
+   const registerCall = () =>{
+       register(nameRef.current,emailRef.current,mobileRef.current,addressRef.current,passwordRef.current);
+    }
+
     return (
         <div className="register-box">
             <h2>Patient Register</h2>
-            <input type="text" placeholder="Enter Name" />
-            <input type="email" placeholder="Enter Email Id" />
-            <input type="text" placeholder="Enter Mobile No" />
-            <input type="text" placeholder="Enter Address"/>
-            <input type="password" placeholder="Enter Password" />
-            <button className="register-submit-btn">Register</button>
+            <input type="text" onChange={(e)=>{nameRef.current=e.target.value;}} placeholder="Enter Name" />
+            <input type="email" onChange={(e)=>{emailRef.current=e.target.value;}} placeholder="Enter Email Id" />
+            <input type="text" onChange={(e)=>{mobileRef.current=e.target.value;}} placeholder="Enter Mobile No" />
+            <input type="text" onChange={(e)=>{addressRef.current=e.target.value;}} placeholder="Enter Address"/>
+            <input type="password" onChange={(e)=>{passwordRef.current=e.target.value;}} placeholder="Enter Password" />
+            <button className="register-submit-btn" onClick={registerCall}>Register</button>
             <p>Already have account?</p>
-            <button className="back-login-btn" onClick={openLogin}>Back To Login</button>
+            <button className="back-login-btn" >Back To Login</button>
         </div>
     );
 }
--- src/services/authenticationService.js
+++ src/services/authenticationService.js
@@ -1,25 +1,28 @@
+import axios from "axios";
+
+// Doctor ands Patient Login
 export  const login = async (type, email, password) => {
-    console.log("email ",email);
-    console.log("password ",password);
-    console.log("type ",type);
     const url = `http://localhost:8080/api/${type}/login`;
-    console.log("url "+url);
     try {
         const inputdata={email,password};
-        const response = await fetch(url,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(inputdata)
-            }
-        );
-        const data = await response.json();
-        console.log(data);
-        alert(`${type} Login Successful`);
+        const response = await axios.post(url,inputdata);
+        console.log(response);
+        alert(`${type} login Successful`);
     } catch (error) {
         console.log(`Error in ${type} login :`, error);
 
+    }
+}
+
+// Patient Register
+export  const register = async (name, email, mobile, address, password) => {
+    const url = `http://localhost:8080/api/patient/register`;
+    try {
+        const inputdata={name, email, mobile, address, password};
+        const response = await axios.post(url,inputdata);
+        console.log(response);
+        alert(`Register Successful`);
+    } catch (error) {
+        console.log(`Error in Register :`, error.message);
     }
 }
